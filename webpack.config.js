ccc45484b07515d297236de4f0cae98434a3b7bc
Luc van den Brand <lucvandenbrand@protonmail.com>
2020-01-23 14:01:06 +0100

    Created cleanup functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,7 @@ module.exports = {
     app: './src/index.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'GGJ20',
     }),
