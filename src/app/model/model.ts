531032e956c5a1aac6b748f8a83d1409746c777d
troido <troido@protonmail.com>
2020-01-31 20:59:37 +0100

    made Vec2 class

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -5,13 +5,32 @@ export function modelmain() {
     
     let a = new HexPos(0, 0);
     console.log(a.get_neighbours());
+    for (let neighbour of a.get_neighbours()){
+        console.log(a.direction_to(neighbour)/ Math.PI)
+    }
+}
+
+
+
+export class Vec2 {
+    x = 0
+    y = 0
     
+    
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    direction_to(other: Vec2){
+        return Math.atan2(this.y - other.y, this.x - other.x);
+    }
     
 }
 
 export class HexPos {
-    x = 0
-    y = 0
+    x = 0;
+    y = 0;
     
     constructor(x: number, y: number) {
         this.x = x;
@@ -19,10 +38,10 @@ export class HexPos {
     }
     
     real_position() {
-        return {
-            x: this.x + this.y * 0.5,
-            y: this.y + Math.sqrt(3/4)
-        }
+        return new Vec2(
+            this.x + this.y * 0.5,
+            this.y * Math.sqrt(3/4)
+        );
     }
     
     get_neighbours() {
@@ -37,7 +56,7 @@ export class HexPos {
     }
     
     direction_to(other: HexPos){
-        return Math.atan2(other.y - this.y, other.x - this.x);
+        return this.real_position().direction_to(other.real_position());
     }
     
     direction_score(target: HexPos, neighbour: HexPos) {
