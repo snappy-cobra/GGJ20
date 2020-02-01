8ca65a3bb8bb1b871562dfe9463fd47de12dc3d6
Gezzellig <knaap@live.com>
2020-02-01 20:23:37 +0100

    added harbors

--- src/app/model/MapMaker.ts
+++ src/app/model/MapMaker.ts
@@ -4,6 +4,7 @@ import {Tile, tiles} from "./Tile";
 import {Forest} from "./MapMaker/Forest";
 import {Farm} from "./MapMaker/Farm";
 import {Grass} from "./MapMaker/Grass";
+import {Road} from "./MapMaker/Road";
 
 enum Fase {
     mountain,
@@ -25,11 +26,13 @@ export class MapMaker extends GameMap{
         }
         super(width, height, ground);
         new Grass(this,5, 300);
-        console.log("Builded Grass")
+        console.log("Builded Grass");
+        new Road(this);
+        console.log("Builded Harbors");
         this.mountainRange = new MountainRange(this, 4,50);
-        console.log("Builded Mountains")
+        console.log("Builded Mountains");
         this.forest = new Forest(this, 8, 70);
-        console.log("Builded Forest")
+        console.log("Builded Forest");
         this.farm = new Farm(this, 1, 15);
         console.log("Builded Farms")
     }
--- src/app/model/Tile.ts
+++ src/app/model/Tile.ts
@@ -10,6 +10,7 @@ export enum TextureType {
     Street = 4,
     Farm = 5,
     Water = 1,
+    Harbor = 6,
 
 
     Street_AA = 1*Y+0,      Street_AB = 1*Y+1,      Street_AC = 1*Y+2,      Street_AD = 1*Y+3,      Street_AE = 1*Y+4,      Street_AF = 1*Y+5, 
@@ -65,6 +66,12 @@ class Ocean extends Tile {
     }
 }
 
+class Harbor extends Tile {
+    constructor(){
+        super("harbor", 0, TextureType.Harbor);
+    }
+}
+
 class StreetHead extends Tile {
     target: HexPos;
     prev: Direction
@@ -83,5 +90,6 @@ export var tiles = {
     Forest,
     Farm,
     StreetHead,
-    Ocean
+    Ocean,
+    Harbor
 };
